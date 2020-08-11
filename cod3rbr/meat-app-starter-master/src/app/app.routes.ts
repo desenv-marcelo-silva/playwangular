e50609d52c9d88c2bca2fbac10edaae60ce02008
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-08-10 23:42:10 -0300

    feat: implement routes to sub navigation menus

--- cod3rbr/meat-app-starter-master/src/app/app.routes.ts
+++ cod3rbr/meat-app-starter-master/src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from "@angular/router";
 
-import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
-import { RestaurantComponent } from "./restaurant/restaurant.component";
-
 import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
+import { RestaurantComponent } from "./restaurant/restaurant.component";
+
+import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
+import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
+import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 
 export const ROUTES: Routes = [
   { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "restaurant", component: RestaurantComponent },
-  { path: "restaurant/:id", component: RestaurantDetailComponent },
+  {
+    path: "restaurant/:id",
+    component: RestaurantDetailComponent,
+    children: [
+      { path: "", redirectTo: "menu", pathMatch: "full" },
+      { path: "menu", component: MenuComponent },
+      { path: "reviews", component: ReviewsComponent },
+    ],
+  },
 ];
