622769a93e8a7b0e0647de0af4388c716f9e00b8
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-08-11 08:51:55 -0300

    feat(shopping-cart-service): create to handle shopping cart

--- cod3rbr/meat-app-starter-master/src/app/app.module.ts
+++ cod3rbr/meat-app-starter-master/src/app/app.module.ts
@@ -7,16 +7,21 @@ import { ROUTES } from "./app.routes";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
+
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+
 import { RestaurantComponent } from "./restaurant/restaurant.component";
 import { RestaurantItemComponent } from "./restaurant/restaurant-item/restaurant-item.component";
-import { RestaurantService } from "./restaurant/restaurant.service";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
+import { RestaurantService } from "./restaurant/restaurant.service";
+
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
-import { ShoppingCartComponent } from "./restaurant-detail/shopping-cart/shopping-cart.component";
 import { MenuItemComponent } from "./restaurant-detail/menu-item/menu-item.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
+
+import { ShoppingCartComponent } from "./restaurant-detail/shopping-cart/shopping-cart.component";
+import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
 
 @NgModule({
   declarations: [
@@ -33,7 +38,7 @@ import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component"
     ReviewsComponent,
   ],
   imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
-  providers: [RestaurantService],
+  providers: [RestaurantService, ShoppingCartService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
