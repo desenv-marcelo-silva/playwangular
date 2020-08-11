75209f1ca839f5d4622e8cc1c632c07d13fc4280
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-08-10 23:27:51 -0300

    feat(restaurant-detail): bind with API

--- cod3rbr/meat-app-starter-master/src/app/restaurant-detail/restaurant-detail.component.ts
+++ cod3rbr/meat-app-starter-master/src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { RestaurantService } from "./../restaurant/restaurant.service";
+
+import { Restaurante } from "./../restaurant/restaurant-item/restaurant-item.model";
 
 @Component({
-  selector: 'mt-restaurant-detail',
-  templateUrl: './restaurant-detail.component.html'
+  selector: "mt-restaurant-detail",
+  templateUrl: "./restaurant-detail.component.html",
 })
 export class RestaurantDetailComponent implements OnInit {
+  restaurante: Restaurante;
 
-  constructor() { }
+  constructor(
+    private restaurantServce: RestaurantService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.restaurantServce
+      .restaurantById(this.route.snapshot.params["id"])
+      .subscribe((restaurante) => (this.restaurante = restaurante));
   }
-
 }
