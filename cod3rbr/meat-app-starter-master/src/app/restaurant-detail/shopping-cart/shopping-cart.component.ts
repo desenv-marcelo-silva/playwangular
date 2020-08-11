857a32c5587c4c4691e59b1c62aa3a437c8a9619
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-08-11 09:01:26 -0300

    feat(shopping-cart): add, remove and clear shopping items

--- cod3rbr/meat-app-starter-master/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ cod3rbr/meat-app-starter-master/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 import { ShoppingCartService } from "./shopping-cart.service";
 import { CartItem } from "./cart-item.model";
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
 
 @Component({
   selector: "mt-shopping-cart",
@@ -14,6 +15,18 @@ export class ShoppingCartComponent implements OnInit {
 
   items(): CartItem[] {
     return this.shoppingCartService.items;
+  }
+
+  clear(): void {
+    this.shoppingCartService.clear();
+  }
+
+  addItem(item: MenuItem): void {
+    this.shoppingCartService.addItem(item);
+  }
+
+  removeItem(item: CartItem): void {
+    this.shoppingCartService.removeItem(item);
   }
 
   total(): number {
