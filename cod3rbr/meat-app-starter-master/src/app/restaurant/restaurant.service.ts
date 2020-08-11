3d87608e6f14246b804335afc89a0c1f4503faa4
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-08-10 23:26:46 -0300

    feat(ErrorHandler): implement and use a generic error handler

--- cod3rbr/meat-app-starter-master/src/app/restaurant/restaurant.service.ts
+++ cod3rbr/meat-app-starter-master/src/app/restaurant/restaurant.service.ts
@@ -1,19 +1,29 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
-import { Restaurante } from './restaurant-item/restaurant-item.model';
-import { MEAT_API } from './../app.api';
+import { MEAT_API } from "./../app.api";
+import { ErrorHandler } from "./../app.error-handler";
+
+import { Restaurante } from "./restaurant-item/restaurant-item.model";
 
 @Injectable()
 export class RestaurantService {
-
   constructor(private http: Http) {}
 
   restaurants(): Observable<Restaurante[]> {
     return this.http
       .get(`${MEAT_API}/restaurants`)
-      .map(response => response.json());
+      .map((response) => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+
+  restaurantById(id: string): Observable<Restaurante> {
+    return this.http
+      .get(`${MEAT_API}/restaurants/${id}`)
+      .map((response) => response.json())
+      .catch(ErrorHandler.handleError);
   }
 }
