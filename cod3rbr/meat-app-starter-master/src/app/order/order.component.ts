d5e14c7edeb5fc0844b1b611708d76089c0eecb7
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-08-13 09:32:29 -0300

    feat(order): check order

--- cod3rbr/meat-app-starter-master/src/app/order/order.component.ts
+++ cod3rbr/meat-app-starter-master/src/app/order/order.component.ts
@@ -3,27 +3,34 @@ import { OrderService } from "./order.service";
 import { Component, OnInit } from "@angular/core";
 
 import { RadioOption } from "./../shared/radio/radio-option.model";
+import { Order, OrderItem } from "./order.model";
 
 @Component({
   selector: "mt-order",
   templateUrl: "./order.component.html",
 })
 export class OrderComponent implements OnInit {
+  delivery: number = 8; // Fix on the future (Frete in Brasil)
+
   paymentOptions: RadioOption[] = [
     { label: "Dinheiro", value: "MON" },
     { label: "Cartão de débito", value: "DEB" },
     { label: "Cartão Refeição", value: "REF" },
   ];
+
   constructor(private orderService: OrderService) {}
 
   ngOnInit() {}
+
+  itemsValue(): number {
+    return this.orderService.itemsValue();
+  }
 
   cartItems(): CartItem[] {
     return this.orderService.cartItems();
   }
 
   increaseQty(item: CartItem) {
-    console.log("chegou no order component");
     this.orderService.increaseQty(item);
   }
 
@@ -33,5 +40,16 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem) {
     this.orderService.remove(item);
+  }
+
+  checkOrder(order: Order) {
+    order.orderItems = this.cartItems().map(
+      (item: CartItem) => new OrderItem(item.quantity, item.menuItem.id)
+    );
+    this.orderService.checkOrder(order).subscribe((orderId: string) => {
+      console.log(`Compra concluida ${orderId}`);
+      this.orderService.clear();
+    });
+    console.log(order);
   }
 }
--- cod3rbr/meat-app-starter-master/src/app/order/order.service.ts
+++ cod3rbr/meat-app-starter-master/src/app/order/order.service.ts
@@ -1,18 +1,32 @@
-import { CartItem } from "./../restaurant-detail/shopping-cart/cart-item.model";
 import { Injectable } from "@angular/core";
+import { Http, Headers, RequestOptions } from "@angular/http";
+
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+
+import { MEAT_API } from "./../app.api";
 
 import { ShoppingCartService } from "./../restaurant-detail/shopping-cart/shopping-cart.service";
 
+import { CartItem } from "./../restaurant-detail/shopping-cart/cart-item.model";
+import { Order } from "./order.model";
+
 @Injectable()
 export class OrderService {
-  constructor(private shoppingCartService: ShoppingCartService) {}
+  constructor(
+    private shoppingCartService: ShoppingCartService,
+    private http: Http
+  ) {}
+
+  itemsValue(): number {
+    return this.shoppingCartService.total();
+  }
 
   cartItems(): CartItem[] {
     return this.shoppingCartService.items;
   }
 
   increaseQty(item: CartItem) {
-    console.log("chegou no order service");
     this.shoppingCartService.increaseQty(item);
   }
 
@@ -22,5 +36,22 @@ export class OrderService {
 
   remove(item: CartItem) {
     this.shoppingCartService.remove(item);
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    return this.http
+      .post(
+        `${MEAT_API}/orders`,
+        JSON.stringify(order),
+        new RequestOptions({ headers })
+      )
+      .map((response) => response.json())
+      .map((order) => order.id);
+  }
+
+  clear() {
+    this.shoppingCartService.clear();
   }
 }
