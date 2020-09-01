8ea28afaf75c18b99f33943a80f787133b398c91
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-09-01 09:23:53 -0300

    build: install and setup animations

--- cod3rbr/meat-app-starter-master/src/app/app.module.ts
+++ cod3rbr/meat-app-starter-master/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, LOCALE_ID } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpModule } from "@angular/http";
 import { RouterModule, PreloadAllModules } from "@angular/router";
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ROUTES } from "./app.routes";
 
 import { AppComponent } from "./app.component";
@@ -39,6 +39,7 @@ import { SharedModule } from "./shared/shared.module";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpModule,
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES, {
