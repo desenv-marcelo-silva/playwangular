f00a8c12fbcf9fbefd0515c3be608709ed22b7c6
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-09-01 10:57:59 -0300

    feat(restaurant-item): how use 'void' state and transitions to animate

--- cod3rbr/meat-app-starter-master/src/app/restaurant/restaurant-item/restaurant-item.component.ts
+++ cod3rbr/meat-app-starter-master/src/app/restaurant/restaurant-item/restaurant-item.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit, Input } from "@angular/core";
-
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from "@angular/animations";
 import { Restaurante } from "./restaurant-item.model";
 
 @Component({
   selector: "mt-restaurant-item",
   templateUrl: "./restaurant-item.component.html",
+  animations: [
+    trigger("restaurantAppeared", [
+      state("ready", style({ opacity: 1 })),
+      transition("void => ready", [
+        style({ opacity: 0, transform: "translate(-30px, -10px)" }),
+        animate("500ms 0s ease-in-out"),
+      ]),
+    ]),
+  ],
 })
 export class RestaurantItemComponent implements OnInit {
+  restaurantState = "ready";
   @Input() restaurant: Restaurante;
 
   constructor() {}
