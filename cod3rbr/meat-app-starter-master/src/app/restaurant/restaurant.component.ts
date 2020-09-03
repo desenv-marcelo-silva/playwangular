ea0c78a821803b544eef47182e952b0b89912f39
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-09-03 09:25:40 -0300

    feat(search-bar): create, configure and animate

--- cod3rbr/meat-app-starter-master/src/app/restaurant/restaurant.component.ts
+++ cod3rbr/meat-app-starter-master/src/app/restaurant/restaurant.component.ts
@@ -1,22 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from "@angular/animations";
 
-import { RestaurantService } from './restaurant.service';
-import { Restaurante } from './restaurant-item/restaurant-item.model';
+import { RestaurantService } from "./restaurant.service";
+import { Restaurante } from "./restaurant-item/restaurant-item.model";
 
 @Component({
-  selector: 'mt-restaurant',
-  templateUrl: './restaurant.component.html'
+  selector: "mt-restaurant",
+  templateUrl: "./restaurant.component.html",
+  animations: [
+    trigger("toggleSearch", [
+      state(
+        "hidden",
+        style({
+          opacity: 0,
+          "max-height": 0,
+        })
+      ),
+      state(
+        "visible",
+        style({
+          opacity: 1,
+          "max-height": "70px",
+          "margin-top": "20px",
+        })
+      ),
+      transition("* => *", animate("250ms 0s ease-in-out")),
+    ]),
+  ],
 })
 export class RestaurantComponent implements OnInit {
-
+  searchBarState = "hidden";
   restaurants: Restaurante[];
 
-  constructor(private restaurantService: RestaurantService) { }
+  constructor(private restaurantService: RestaurantService) {}
 
   ngOnInit() {
     this.restaurantService
       .restaurants()
-      .subscribe(listaRestaurante => this.restaurants = listaRestaurante );
+      .subscribe((listaRestaurante) => (this.restaurants = listaRestaurante));
   }
 
+  toggleSearch(): void {
+    this.searchBarState =
+      this.searchBarState === "hidden" ? "visible" : "hidden";
+  }
 }
