5a9fb61306d32608c8f5f35345e1e78b4db5ff55
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-09-11 10:10:25 -0300

    chore(server): remove https to test app without certificate

--- cod3rbr/meat-app-starter-master/backend/dist/server.js
+++ cod3rbr/meat-app-starter-master/backend/dist/server.js
@@ -1,8 +1,8 @@
 "use strict";
 exports.__esModule = true;
 var jsonServer = require("json-server");
-var fs = require("fs");
-var https = require("https");
+// import * as fs from "fs";
+// import * as http from "http";
 var auth_1 = require("./auth");
 var authz_1 = require("./authz");
 var server = jsonServer.create();
@@ -17,10 +17,13 @@ server.post("/login", auth_1.handleAuthentication);
 server.use("/orders", authz_1.handleAuthorization);
 // Use default router
 server.use(router);
-var options = {
-    cert: fs.readFileSync("./backend/keys/cert.pem"),
-    key: fs.readFileSync("./backend/keys/key.pem")
-};
-https.createServer(options, server).listen(4205, function () {
-    console.log("JSON Server is running on https://localhost:4205");
+// const options = {
+//   cert: fs.readFileSync("./backend/keys/cert.pem"),
+//   key: fs.readFileSync("./backend/keys/key.pem"),
+// };
+// https.createServer(options, server).listen(4205, () => {
+//   console.log("JSON Server is running on https://localhost:4205");
+// });
+server.listen(4205, function () {
+    console.log("JSON Server is running on http://localhost:4205");
 });
--- cod3rbr/meat-app-starter-master/backend/server.ts
+++ cod3rbr/meat-app-starter-master/backend/server.ts
@@ -1,8 +1,8 @@
-import * as jsonServer from "json-server";
 import { Express } from "express";
 
-import * as fs from "fs";
-import * as https from "https";
+import * as jsonServer from "json-server";
+// import * as fs from "fs";
+// import * as http from "http";
 
 import { handleAuthentication } from "./auth";
 import { handleAuthorization } from "./authz";
@@ -23,11 +23,15 @@ server.use("/orders", handleAuthorization);
 // Use default router
 server.use(router);
 
-const options = {
-  cert: fs.readFileSync("./backend/keys/cert.pem"),
-  key: fs.readFileSync("./backend/keys/key.pem"),
-};
+// const options = {
+//   cert: fs.readFileSync("./backend/keys/cert.pem"),
+//   key: fs.readFileSync("./backend/keys/key.pem"),
+// };
 
-https.createServer(options, server).listen(4205, () => {
-  console.log("JSON Server is running on https://localhost:4205");
+// https.createServer(options, server).listen(4205, () => {
+//   console.log("JSON Server is running on https://localhost:4205");
+// });
+
+server.listen(4205, () => {
+  console.log("JSON Server is running on http://localhost:4205");
 });
