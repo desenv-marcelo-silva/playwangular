3b8c9cdc9b63d68d1d27578726cb596ac50f6c94
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-09-11 09:01:17 -0300

    feat(login): add reactive forms

--- cod3rbr/meat-app-starter-master/src/app/security/login/login.component.ts
+++ cod3rbr/meat-app-starter-master/src/app/security/login/login.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'mt-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "mt-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: this.fb.control("", [Validators.required, Validators.email]),
+      password: this.fb.control("", [Validators.required]),
+    });
   }
-
 }
