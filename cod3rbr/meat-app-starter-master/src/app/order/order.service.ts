b06b8262499d073f9631fa839f1369a45587c725
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-09-14 10:15:33 -0300

    reafactor(order): use interceptor to send token

--- cod3rbr/meat-app-starter-master/src/app/order/order.service.ts
+++ cod3rbr/meat-app-starter-master/src/app/order/order.service.ts
@@ -7,7 +7,6 @@ import "rxjs/add/operator/map";
 import { MEAT_API } from "./../app.api";
 
 import { ShoppingCartService } from "./../restaurant-detail/shopping-cart/shopping-cart.service";
-import { LoginService } from "../security/login/login.service";
 
 import { CartItem } from "./../restaurant-detail/shopping-cart/cart-item.model";
 import { Order } from "./order.model";
@@ -16,7 +15,6 @@ import { Order } from "./order.model";
 export class OrderService {
   constructor(
     private shoppingCartService: ShoppingCartService,
-    private loginService: LoginService,
     private http: HttpClient
   ) {}
 
@@ -41,16 +39,8 @@ export class OrderService {
   }
 
   checkOrder(order: Order): Observable<string> {
-    let headers = new HttpHeaders();
-    if (this.loginService.isLoggedIn()) {
-      headers = headers.set(
-        "Authorization",
-        `Bearer ${this.loginService.user.accessToken}`
-      );
-    }
-
     return this.http
-      .post<Order>(`${MEAT_API}/orders`, order, { headers })
+      .post<Order>(`${MEAT_API}/orders`, order)
       .map((order) => order.id);
   }
 
