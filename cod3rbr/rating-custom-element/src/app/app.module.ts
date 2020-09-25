fe0d3c787c1fd2e0ee2b5ae23aa2bbc71a05c525
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-09-25 11:47:01 -0300

    chore: create, register and start custom elements

--- cod3rbr/rating-custom-element/src/app/app.module.ts
+++ cod3rbr/rating-custom-element/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { RatingComponent } from './rating.component';
 
 @NgModule({
   declarations: [RatingComponent],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [RatingComponent],
+  entryComponents: [RatingComponent], // this component will be used in some moment
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {
+    const component = createCustomElement(RatingComponent, { injector });
+
+    customElements.define('mt-rating', component);
+  }
+
+  ngDoBootstrap() {}
+}
